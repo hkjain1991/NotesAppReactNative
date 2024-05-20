cee15096092fb86506b367185413f67041b28f4c
hemeandra <hemeandrajain>
2024-05-20 18:45:24 +0530

    Added navigator

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import Dashboard from './Dashboard';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import NotesEditOrCreate from './NotesEditOrCreate';
 const App = () => {
-  return (<Dashboard/>);
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Dashboard">
+        <Stack.Screen name="Dashboard" component={Dashboard} />
+        <Stack.Screen name="NotesEdit" component={NotesEditOrCreate} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 export default App;
